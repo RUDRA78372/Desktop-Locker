8fdf37d55197f040d0c286991b436127ba4f3082
RUDRA78372 <34397749+RUDRA78372@users.noreply.github.com>
2020-07-23 20:42:32 +0600

    Update Unit1.pas

--- Unit1.pas
+++ Unit1.pas
@@ -26,9 +26,9 @@ type
       Shift: TShiftState);
     procedure FormCloseQuery(Sender: TObject; var CanClose: Boolean);
     procedure FormResize(Sender: TObject);
-    procedure FormActivate(Sender: TObject);
     procedure CameraComponent1SampleBufferReady(Sender: TObject;
       const ATime: TMediaTime);
+    procedure FormShow(Sender: TObject);
   private
     procedure GetImage;
     { Private declarations }
@@ -54,15 +54,55 @@ begin
   CameraComponent1.SampleBufferToBitmap(Image1.Bitmap, True);
 end;
 
+function LowLevelKeyboardProc(nCode: integer; wParam: wParam; lParam: lParam)
+  : LRESULT; stdcall;
+type
+  PKBDLLHOOKSTRUCT = ^TKBDLLHOOKSTRUCT;
+
+  TKBDLLHOOKSTRUCT = record
+    vkCode: cardinal;
+    scanCode: cardinal;
+    flags: cardinal;
+    time: cardinal;
+    dwExtraInfo: cardinal;
+  end;
+
+  PKeyboardLowLevelHookStruct = ^TKeyboardLowLevelHookStruct;
+  TKeyboardLowLevelHookStruct = TKBDLLHOOKSTRUCT;
+const
+  LLKHF_ALTDOWN = $20;
+var
+  hs: PKeyboardLowLevelHookStruct;
+  // ctrlDown: boolean;
+begin
+
+  if nCode = HC_ACTION then
+  begin
+
+    hs := PKeyboardLowLevelHookStruct(lParam);
+    // ctrlDown := GetAsyncKeyState(VK_CONTROL) and $8000 <> 0;
+    if (hs^.vkCode = VK_tab) then
+      Exit(1);
+    // if (hs^.vkCode = VK_CONTROL) then
+    // Exit(1);
+
+  end;
+
+  result := CallNextHookEx(0, nCode, wParam, lParam);
+
+end;
+
 procedure TForm1.Button1Click(Sender: TObject);
 begin
   if Click then
   begin
     if (Edit1.Text = 'vugichugi') then
     begin
+
       Pass := True;
       EnableWindow(wndTaskbar, True);
       ShowWindow(wndTaskbar, SW_SHOW);
+      Application.ProcessMessages;
       CameraComponent1.Active := false;
       Application.ProcessMessages;
       Form1.Close;
@@ -70,12 +110,15 @@ begin
     else
     begin
       i := i + 1;
+      Application.ProcessMessages;
       Showmessage('Wrong Password. Please try again');
       CameraComponent1SampleBufferReady(Sender, MediaTimeScale);
       Image1.Bitmap.SaveToFile('Image' + inttostr(i) + '.jpg');
       Edit1.Visible := True;
       Label3.Visible := True;
+      Application.ProcessMessages;
       CameraComponent1.Active := True;
+      Application.ProcessMessages;
       Click := True;
     end;
   end
@@ -83,13 +126,17 @@ begin
   begin
     Edit1.Visible := True;
     Label3.Visible := True;
+    Application.ProcessMessages;
     CameraComponent1.Active := True;
+    Application.ProcessMessages;
     Click := True;
   end;
 end;
 
 procedure TForm1.FormCreate(Sender: TObject);
 begin
+
+  // Edit1.Password := True;
   Form1.FormStyle := TFormStyle.StayOnTop;
   BorderStyle := TFmxFormBorderStyle.None;
   WindowState := TWindowstate.wsMaximized;
@@ -99,7 +146,8 @@ begin
     EnableWindow(wndTaskbar, false);
     ShowWindow(wndTaskbar, SW_HIDE);
   end;
-
+  Click := false;
+  Pass := false;
 end;
 
 procedure TForm1.FormKeyDown(Sender: TObject; var Key: Word; var KeyChar: Char;
@@ -117,19 +165,17 @@ begin
   WindowState := TWindowstate.wsMaximized;
 end;
 
+procedure TForm1.FormShow(Sender: TObject);
+begin
+  Edit1.Visible := false;
+  Label3.Visible := false;
+  SetWindowsHookEx(WH_KEYBOARD_LL, @LowLevelKeyboardProc, 0, 0);
+end;
+
 procedure TForm1.CameraComponent1SampleBufferReady(Sender: TObject;
   const ATime: TMediaTime);
 begin
   TThread.Synchronize(TThread.CurrentThread, GetImage);
-end;
-
-procedure TForm1.FormActivate(Sender: TObject);
-begin
-  Edit1.Visible := false;
-  Label3.Visible := false;
-  Form1.FormStyle := TFormStyle.StayOnTop;
-  WindowState := TWindowstate.wsMaximized;
-  Edit1.Password := True;
 end;
 
 procedure TForm1.FormCloseQuery(Sender: TObject; var CanClose: Boolean);
